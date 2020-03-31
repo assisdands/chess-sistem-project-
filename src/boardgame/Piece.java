8b2b43666c595471ab43806c8b5f0a96defb26a1
Dandara de Assis <dandara.assis@al.resilia.com.br>
2020-03-31 16:15:26 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,19 @@ package boardgame;
 
 public class Piece {
 
+	protected Position position;
+	
+	private Board boart;
+
+	public Piece(Board boart) {
+		this.boart = boart;
+		position = null;
+	}
+
+	protected Board getBoart() {
+		return boart;
+	}
+
+	
+	
 }
