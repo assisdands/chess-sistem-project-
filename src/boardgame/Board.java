1f055e9a0ff33be11ca12a9219853674ccec1709
Dandara de Assis <dandara.assis@al.resilia.com.br>
2020-04-02 12:42:04 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,6 +1,5 @@
 package boardgame;
-import boardgame.Position;
-import chess.Color;
+
 public class Board {
 
 	private int rows;
@@ -39,6 +38,18 @@ public class Board {
 		
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	public Piece removePiece (Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) {
